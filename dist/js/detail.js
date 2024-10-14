11ec57ec065c4dd0fd667a1e63bd5f10a936f55b
Deryck41 <fegonik1@gmail.com>
2024-10-14 17:47:52 +0300

    deploy only

--- dist/js/detail.js
+++
@@ -1,8 +0,0 @@
-const modal = document.querySelector('.apartments .modal');
-document.querySelector('.model-apart-open').addEventListener('click', () => {
-    modal.classList.add('modal_active');
-});
-
-document.querySelector('.modal-close').addEventListener('click', () => {
-    modal.classList.remove('modal_active');
-});
